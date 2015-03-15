73bfd6b0d812c30d861ac50aa4cbe75c0c9497f3
unknown <arefinkhan@arefin.(none)>
2015-03-16 01:54:09 +0600

    code complete. output mismatch

--- 1_naive_bayesian_classifier/naive_bayesian.cpp
+++ 1_naive_bayesian_classifier/naive_bayesian.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <queue>
 using namespace std;
+#define pi 3.1416
 
 struct info
 {
@@ -81,17 +82,26 @@ void calculate_variance()
             sum += (ii.PW[j] - ii.avgPW)*(ii.PW[j] - ii.avgPW);
         }
         ii.varPW = sqrt(sum / ii.ndata);
+        //printf("%f %f %f %f\n",ii.varPL,ii.varPW,ii.varSL,ii.varSW);
         data[i] = ii;
     }
 }
 
+double denominator(double variance)
+{
+    return sqrt(2*pi) * variance;
+}
+double numerator(double variance,double X,double average)
+{
+    return exp(-1*(X-average)*(X-average)/(2*variance*variance));
+}
 void print_info()
 {
     int i;
     for(i=0; i<3; i++)
     {
         info ii = data[i];
-        printf("%lf %lf %lf %lf\n",ii.avgPL,ii.avgPW,ii.avgSL,ii.avgSW);
+        printf("%f %f %f %f\n",ii.avgPL,ii.avgPW,ii.avgSL,ii.avgSW);
     }
 }
 
@@ -114,14 +124,65 @@ int main()
         * d => PW
         * e =>CLASS
         */
-        printf("%lf %lf %lf %lf %d\n",a,b,c,d,e);
+        //printf("%f %f %f %f %d\n",a,b,c,d,e);
         info ii = data[e];
         ii.SL.push_back(a),ii.SW.push_back(b),ii.PL.push_back(c),ii.PW.push_back(d);
+        data[e] = ii;
     }
-
+    fclose(stdin);
     calculate_average();
     calculate_variance();
-    print_info();
+    //print_info();
+
+    freopen("set_test.txt","r",stdin);
+    double maxx;
+    pair <double,int> ans;
+    ans.first = 0;
+    ans.second = -1;
+    while(scanf("%lf %lf %lf %lf %d",&a,&b,&c,&d,&e))
+    {
+        // 1/sqrt(2*pi*variance) * e ^ -(X-mean)^2/2*variance^2
+        /*
+        * a => SL
+        * b => SW
+        * c => PL
+        * d => PW
+        * e =>CLASS
+        */
+        maxx = 0;
+        int i;
+        for(i=0; i<3; i++)
+        {
+            info ii = data[i];
+            double d = denominator(ii.varSL);
+            double n = numerator(ii.varSL,a,ii.avgSL);
+            //printf("%f %f %f\n",d,n,ii.varSL);
+            double resSL = n/d;
+            d = denominator(ii.varSW);
+            n = numerator(ii.varSW,b,ii.avgSW);
+            double resSW = n/d;
+            d = denominator(ii.varPL);
+            n = numerator(ii.varPL,c,ii.avgPL);
+            double resPL = n/d;
+            d = denominator(ii.varPW);
+            n = numerator(ii.varPW,c,ii.avgPW);
+            double resPW = n/d;
+            printf("%f %f %f\n",d,n,resPW);
+
+            double res = resSL * resSW * resPL * resPW * ii.ndata;
+            printf("res %f %f %f %f %f\n",res,resSL,resSW,resPL,resPW);
+            if(res > maxx)
+            {
+                ans.first = res;
+                ans.second = i;
+                maxx = res;
+            }
+        }
+
+        //printf("Real %d,Estimated %d\n",e,ans.second);
+    }
+    fclose(stdin);
+
 
     return 0;
 }
